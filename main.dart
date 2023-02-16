fce238e867d2cd74c3a211de8681d6a479b39d00
Adryan-Maikel <100433470+Adryan-Maikel@users.noreply.github.com>
2023-02-16 09:53:37 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,5 +1,9 @@
 import 'dart:io';
 void main(){
+
+  // choiceUser();
+}
+choiceUser(){
   print("Digite o número da atividade");
   print("1- Atividade 1");
   print("2- Atividade 2");
@@ -14,7 +18,7 @@ void main(){
       arrayDivision();
       break;
     case "3":
-
+      evenOrOdd();
       break;
     case "4":
 
@@ -27,7 +31,7 @@ chooseAgain(){
   print("Deseja tentar outra atividade? S - sim");
   String? choice = stdin.readLineSync().toString();
   if(choice.toUpperCase() == "S"){
-    main();
+    choiceUser();
   }
   return;
 }
@@ -49,10 +53,56 @@ arrayDivision(){
   double number = double.parse(stdin.readLineSync()!);
   numbers.add(number);
   for(int i = 0; i < 100; i++){
-    // print(i);
     number /= 2;
     numbers.add(number.toStringAsFixed(2));
   }
   print(numbers);
+  chooseAgain();
+}
+evenOrOdd(){
+  print("Digite 10 valores para separar por par ou impar:");
+  print("Digite o primeiro número:");
+  int numberOne = int.parse(stdin.readLineSync()!);
+  print("Digite o segundo número:");
+  int numberTwo = int.parse(stdin.readLineSync()!);
+  print("Digite o terceiro número:");
+  int numberThird = int.parse(stdin.readLineSync()!);
+  print("Digite o quarto número:");
+  int numberFourth = int.parse(stdin.readLineSync()!);
+  print("Digite o quinto número:");
+  int numberFifth = int.parse(stdin.readLineSync()!);
+  print("Digite o sesto número:");
+  int numberSixth = int.parse(stdin.readLineSync()!);
+  print("Digite o setimo número:");
+  int numberSeventh  = int.parse(stdin.readLineSync()!);
+  print("Digite o oitavo número:");
+  int numberEighth = int.parse(stdin.readLineSync()!);
+  print("Digite o nono número:");
+  int numberNineth = int.parse(stdin.readLineSync()!);
+  print("Digite o decimo número:");
+  int numberTenth = int.parse(stdin.readLineSync()!);
+  var numberEven = [];
+  var numberOdd = [];
+  var numbers = [];
+  numbers.add(numberOne);
+  numbers.add(numberTwo);
+  numbers.add(numberThird);
+  numbers.add(numberFourth);
+  numbers.add(numberFifth);
+  numbers.add(numberSixth);
+  numbers.add(numberSeventh);
+  numbers.add(numberEighth);
+  numbers.add(numberNineth);
+  numbers.add(numberTenth);
+  
+  for(int i = 0; i < 10; i++){
+    if(numbers[i] % 2 == 1){
+      numberEven.add(numbers[i]);
+    } else {
+      numberOdd.add(numbers[i]);
+    }
+  }
+  print("Impares $numberEven");
+  print("Pares $numberOdd");
   chooseAgain();
 }
